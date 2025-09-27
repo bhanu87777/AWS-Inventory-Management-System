e2575e8a4c2a7e86cfa4f518b46bc401b989c418
Bhanu Prakash <bhanump777@example.com>
2025-09-27 06:23:18 +0530

    Initial Commit

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        hostname:
+          "s3-inventorymanagementapplication.s3.ap-south-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
